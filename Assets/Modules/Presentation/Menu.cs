ad5df892755147f59e06d2fb1e7454e9b56c098d
PavelAltynnikov <robenzon11@gmail.com>
2023-08-13 23:04:15 +0400

    update Menu class

--- Assets/Modules/Presentation/Menu.cs
+++ Assets/Modules/Presentation/Menu.cs
@@ -1,17 +1,28 @@
-﻿using UnityEngine;
+﻿#nullable enable
+using UnityEngine;
 
 namespace SpaceSumo.Presentation
 {
     public class Menu : MonoBehaviour
     {
-        public void Show()
+        protected Menu? _previousMenu;
+
+        public void Show(Menu? previousMenu = null)
         {
+            _previousMenu = previousMenu;
+            _previousMenu?.Hide();
             gameObject.SetActive(true);
         }
 
         public void Hide()
         {
             gameObject.SetActive(false);
+        }
+
+        public void Back()
+        {
+            Hide();
+            _previousMenu?.Show(this);
         }
     }
 }
