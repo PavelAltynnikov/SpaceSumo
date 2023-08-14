903a35e1e31f62a6f7d28ae2d60c82445e5336f0
PavelAltynnikov <robenzon11@gmail.com>
2023-08-14 23:41:40 +0400

    add enemy goes out event args

---
+++ Assets/Modules/Game/EnemyGoesOutOfBoundEventArgs.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using System;
+
+namespace SpaceSumo.Game
+{
+    public class EnemyGoesOutOfBoundEventArgs : EventArgs
+    {
+        public Enemy Enemy { get; }
+
+        public EnemyGoesOutOfBoundEventArgs(Enemy enemy)
+        {
+            Enemy = enemy;
+        }
+    }
+}
