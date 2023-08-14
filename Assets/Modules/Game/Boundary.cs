e444bee80f6f5ad3853e2667498416160366fd9e
PavelAltynnikov <robenzon11@gmail.com>
2023-08-14 23:41:02 +0400

    add boundary class

---
+++ Assets/Modules/Game/Boundary.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+using System;
+using UnityEngine;
+
+namespace SpaceSumo.Game
+{
+    public class Boundary : MonoBehaviour
+    {
+        public event Action? PlayerGoesOutOfBounds;
+        public event EventHandler<EnemyGoesOutOfBoundEventArgs>? EnemyGoesOutOfBounds;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(typeof(Player), out Component player))
+            {
+                PlayerGoesOutOfBounds?.Invoke();
+            }
+
+            if (other.TryGetComponent(typeof(Enemy), out Component enemy))
+            {
+                EnemyGoesOutOfBounds?.Invoke(this, new EnemyGoesOutOfBoundEventArgs(enemy as Enemy));
+            }
+        }
+    }
+}
